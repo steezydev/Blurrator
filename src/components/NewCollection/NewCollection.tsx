fa9012ad253e9b80f041eaff671b3ec68e001ec3
Ivan Ilichev <swggnuko.dev@gmail.com>
2023-04-10 16:42:23 +0300

    feat: :sparkles: added new collection form validation

--- src/components/NewCollection/NewCollection.tsx
+++ src/components/NewCollection/NewCollection.tsx
@@ -1,28 +1,27 @@
 import React, { useState } from 'react';
 import { Button, Drawer, TextInput } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
 
 import { useDispatch } from 'react-redux';
 import { addCollection } from '@/reducers/collectionsSlice';
+import { isAddress } from 'ethers';
 
 export default function NewCollection() {
   const [opened, { open, close }] = useDisclosure(false);
   const dispatch = useDispatch();
 
-  const [name, setName] = useState('');
-  const [address, setAddress] = useState('');
+  const form = useForm({
+    initialValues: { name: '', address: '' },
+    validate: {
+      name: (value) => (value.length <= 0 ? 'Name is too short' : null),
+      address: (value) =>
+        !isAddress(value) ? 'Enter a valid contract address' : null,
+    },
+  });
 
-  const handleAdd = () => {
-    if (name === '') {
-      return false;
-    }
-
-    // TODO: Validate address
-    if (address === '') {
-      return false;
-    }
-
-    dispatch(addCollection({ name, address }));
+  const handleAdd = (values: { name: string; address: string }) => {
+    dispatch(addCollection({ name: values.name, address: values.address }));
     close();
   };
 
@@ -34,28 +33,28 @@ export default function NewCollection() {
         onClose={close}
         title='Add collection'
       >
-        <div className='flex flex-col gap-3'>
-          <TextInput
-            value={name}
-            onChange={(event) => setName(event.currentTarget.value)}
-            placeholder='Name'
-            label='Name'
-          />
-          <TextInput
-            value={address}
-            onChange={(event) => setAddress(event.currentTarget.value)}
-            placeholder='Address'
-            label='Address'
-          />
-        </div>
-        <Button
-          disabled={name === '' || address === ''}
-          fullWidth
-          className='mt-4'
-          onClick={handleAdd}
-        >
-          Add
-        </Button>
+        <form onSubmit={form.onSubmit((values) => handleAdd(values))}>
+          <div className='flex flex-col gap-3'>
+            <TextInput
+              {...form.getInputProps('name')}
+              placeholder='Name'
+              label='Name'
+            />
+            <TextInput
+              {...form.getInputProps('address')}
+              placeholder='Address'
+              label='Address'
+            />
+          </div>
+          <Button
+            disabled={form.values.name === '' || form.values.address === ''}
+            fullWidth
+            className='mt-4'
+            type='submit'
+          >
+            Add
+          </Button>
+        </form>
       </Drawer>
       <Button onClick={open}>Add collection</Button>
     </>
